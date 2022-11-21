e8c8316d0bdf924230b3a502dbb26b63bbd818d4
deepscape <hdkim.scala@gmail.com>
2022-11-21 18:43:42 +0900

    chapter03 BaseTimeEntity finish

---
+++ src/main/java/com/jojoldu/book/springboot/domain/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.jojoldu.book.springboot.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass       // Posts 가 상속받을 때, 아래 필드가 칼럼으로 인식되도록
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
